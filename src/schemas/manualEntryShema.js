bea36c14954e89b2aa096add36c33f41530b1699
gougisle <lefebagougis@gmail.com>
2024-02-29 17:40:45 -0800

    refactored Manual Form to use concise list of services, connected app to new sheets.best connection, updated manualSchema and added new constant CONCISE_SERVCIES_ARRAY

--- src/schemas/manualEntryShema.js
+++ src/schemas/manualEntryShema.js
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 const manualEntrySchema = Yup.object().shape({
   name: Yup.string().required("Please provide the client's name"),
-  jobType: Yup.string().required(
+  service: Yup.string().required(
     "Please select the service/job being provided."
   ),
   address: Yup.string().required("Please provide the address of this job."),
